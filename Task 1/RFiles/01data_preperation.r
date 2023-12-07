8becca212ab4ad922988783c28746018d3c61cd7
mariana.nold <mariana@die-nolds.de>
2023-12-07 09:45:58 +0100

    commit

--- Task 1/RFiles/01data_preperation.r
+++ Task 1/RFiles/01data_preperation.r
@@ -3,9 +3,9 @@ IMPDAT <- subset(IMPDAT,IMPDAT$CNT == "AUT")
 school.id <- IMPDAT$CNTSCHID
 pisa18 <- data.frame(school.id)
 pisa18$CNT  <- IMPDAT$CNT
-LD <- as.numeric(IMPDAT$PV1READ < 407)
+pisa18$ld <- as.numeric(IMPDAT$PV1READ < 407)
 # Outcome, Fraction of literary deprived students
-pisa18$LDFRAC <- ave(LD, school.id, FUN = function(x) mean(x, na.rm = TRUE))
+pisa18$LD <- ave(pisa18$ld, school.id, FUN = function(x) mean(x, na.rm = TRUE))
 
 #Parent support, 1 = Strongly disagree, to 4 = Strongly agree
 parent_sup1 <- IMPDAT$ST123Q02NA # My parents support my educational efforts and achievements.
@@ -14,36 +14,37 @@ parent_sup3 <- IMPDAT$ST123Q04NA # My parents encourage me to be confident.
 
 parent_sup <- data.frame(parent_sup1,parent_sup2,parent_sup3)
 mean.parent_sup <- apply(parent_sup,FUN = mean, MARGIN = 1, na.rm = TRUE)
-parent_sup_sc <- scale(mean.parent_sup)[,1]
-pisa18$PARENT_SUP <- ave(parent_sup_sc, school.id, FUN = function(x) mean(x, na.rm = TRUE))
+pisa18$parent_sup <- scale(mean.parent_sup)[,1]
+pisa18$PARENT_SUP <- ave(pisa18$parent_sup, school.id, FUN = function(x) mean(x, na.rm = TRUE))
 
 # Gender PISA
 gender_pisa <- IMPDAT$ST004D01T
-FEMALE <- rep(0, length(gender_pisa))
-FEMALE[gender_pisa == 1] <- 1
-pisa18$FEMFRAC_ST <- ave(FEMALE, school.id, FUN = function(x) mean(x, na.rm = TRUE))
+pisa18$female <- rep(0, length(gender_pisa))
+pisa18$female[gender_pisa == 1] <- 1
+pisa18$FEMFRAC_ST <- ave(pisa18$female, school.id, FUN = function(x) mean(x, na.rm = TRUE))
 # NATIVE
 lang_home <- IMPDAT$ST022Q01TA
-NATIVE <- rep(0,length(lang_home))
-NATIVE[lang_home == 1] <- 1
-pisa18$NATIVEFRAC <- ave(NATIVE, school.id, FUN = function(x) mean(x, na.rm = TRUE))
+pisa18$native <- rep(0,length(lang_home))
+pisa18$native[lang_home == 1] <- 1
+pisa18$native <- ave(pisa18$native, school.id, FUN = function(x) mean(x, na.rm = TRUE))
 # IMMIG
-immig <- IMPDAT$IMMIG
-immig[IMPDAT$IMMIG == 1] <- 0
-immig[IMPDAT$IMMIG > 1] <- 1
-pisa18$IMMIGFRAC <- ave(immig, school.id, FUN = function(x) mean(x, na.rm = TRUE))
+pisa18$immig <- IMPDAT$IMMIG
+pisa18$immig[IMPDAT$IMMIG == 1] <- 0
+pisa18$immig[IMPDAT$IMMIG > 1] <- 1
+pisa18$IMMIG <- ave(pisa18$immig, school.id, FUN = function(x) mean(x, na.rm = TRUE))
 
-ESCS <- scale(IMPDAT$ESCS)[,1]
-pisa18$ESCS <- ave(ESCS, school.id, FUN = function(x) mean(x, na.rm = TRUE))
+pisa18$escs <- scale(IMPDAT$ESCS)[,1]
+pisa18$ESCS <- ave(pisa18$escs, school.id, FUN = function(x) mean(x, na.rm = TRUE))
 
 books <- IMPDAT$ST013Q01TA
 # 25 books maximum -> rename few_books
-few_book <- rep(0,length(books))
-few_book[books < 3] <- 1
-pisa18$FEWBOOKSFRAC <- ave(few_book, school.id, FUN = function(x) mean(x, na.rm = TRUE))
+pisa18$fewbooks <- rep(0,length(books))
+pisa18$fewbooks[books < 3] <- 1
+pisa18$FEWBOOKS <- ave(pisa18$fewbooks, school.id, FUN = function(x) mean(x, na.rm = TRUE))
 
-repeatclass <- IMPDAT$REPEAT
-pisa18$REPEATFRAC <- ave(repeatclass, school.id, FUN = function(x) mean(x, na.rm = TRUE))
+pisa18$repeatclass <- IMPDAT$REPEAT
+pisa18$REPEATCLASS <- ave(pisa18$repeatclass, school.id, FUN = function(x) mean(x, na.rm = TRUE))
+
 
 # Disciplinary climate # 1 = Every lesson to 4 = Never or hardly ever
 dis1  <- IMPDAT$ST097Q01TA  # Students don't listen to what the teacher says.
@@ -54,8 +55,8 @@ dis5  <- IMPDAT$ST097Q05TA  # Students don't start working for a long time after
 
 dis_clim <- data.frame(dis1,dis2,dis3,dis4,dis5)
 mean.dis_clim <- apply(dis_clim,FUN = mean, MARGIN = 1, na.rm = TRUE)
-dis_clim_sc <- scale(mean.dis_clim)[,1]
-pisa18$DISCLIMA <- ave(dis_clim_sc, school.id, FUN = function(x) mean(x, na.rm = TRUE))
+pisa18$disclima <- scale(mean.dis_clim)[,1]
+pisa18$DISCLIMA <- ave(pisa18$disclima, school.id, FUN = function(x) mean(x, na.rm = TRUE))
 
 # Teacher support # 1 = every lesson,..4 = never or hardly ever
 teachsupp1  <- 5 - IMPDAT$ST100Q01TA  # The teacher shows an interest in every student's learning.
@@ -65,8 +66,8 @@ teachsupp4  <- 5 - IMPDAT$ST100Q04TA  # The teacher continues teaching until the
 
 teachsupp <- data.frame(teachsupp1,teachsupp2,teachsupp3,teachsupp4)
 mean.teachsupp <- apply(teachsupp,FUN = mean, MARGIN = 1, na.rm = TRUE)
-teachsupp_sc <- scale(mean.teachsupp)[,1]
-pisa18$TEACHSUP <- ave(teachsupp_sc, school.id, FUN = function(x) mean(x, na.rm = TRUE))
+pisa18$teachsup <- scale(mean.teachsupp)[,1]
+pisa18$TEACHSUP <- ave(pisa18$teachsup, school.id, FUN = function(x) mean(x, na.rm = TRUE))
 
 # Teacher feedback # 1 = never or hardly ever,..4 = every lesson
 teachfeed1  <- IMPDAT$ST104Q02NA  # The teacher gives me feedback on my strengths in this subject.
@@ -75,8 +76,8 @@ teachfeed3  <- IMPDAT$ST104Q04NA  # The teacher tells me how I can improve my pe
 
 teachfeed <- data.frame(teachfeed1,teachfeed2,teachfeed3)
 mean.teachfeed <- apply(teachfeed,FUN = mean, MARGIN = 1, na.rm = TRUE)
-teachfeed_sc <- scale(mean.teachfeed)[,1]
-pisa18$PERFEED <- ave(teachfeed_sc, school.id, FUN = function(x) mean(x, na.rm = TRUE))
+pisa18$perfeed <- scale(mean.teachfeed)[,1]
+pisa18$PERFEED <- ave(pisa18$perfeed, school.id, FUN = function(x) mean(x, na.rm = TRUE))
 
 # Teacher-directed instruction  # 1 = every lesson,..4 = never or hardly ever
 dirins1 <- 5 - IMPDAT$ST102Q01TA # The teacher sets clear goals for our learning.
@@ -86,8 +87,8 @@ dirins4 <- 5 - IMPDAT$ST102Q04TA # The teacher tells us what we have to learn.
 
 dirins <- data.frame(dirins1,dirins2,dirins3,dirins4)
 mean.dirins <- apply(dirins,FUN = mean, MARGIN = 1, na.rm = TRUE)
-dirins_sc <- scale(mean.dirins)[,1]
-pisa18$DIRINS <- ave(dirins_sc, school.id, FUN = function(x) mean(x, na.rm = TRUE))
+pisa18$dirins <- scale(mean.dirins)[,1]
+pisa18$DIRINS <- ave(pisa18$dirins, school.id, FUN = function(x) mean(x, na.rm = TRUE))
 
 # Adaptive instruction,  # 1 = never or hardly ever,..4 = every lesson
 adaptive1  <- IMPDAT$ST212Q01HA  # The teacher adapts the lesson to my class’s needs and knowledge.
@@ -96,8 +97,8 @@ adaptive3  <- IMPDAT$ST212Q03HA  # The teacher changes the structure of the less
 
 adaptive <- data.frame(adaptive1,adaptive2,adaptive3)
 mean.adaptive <- apply(adaptive,FUN = mean, MARGIN = 1, na.rm = TRUE)
-adaptive_sc <- scale(mean.adaptive)[,1]
-pisa18$ADAPTIVE <- ave(adaptive_sc, school.id, FUN = function(x) mean(x, na.rm = TRUE))
+pisa18$adaptive <- scale(mean.adaptive)[,1]
+pisa18$ADAPTIVE <- ave(pisa18$adaptive, school.id, FUN = function(x) mean(x, na.rm = TRUE))
 
 # Teacher enthusiasm, strongly disagree = 1, strongly agree = 4
 enthus1 <- IMPDAT$ST213Q01HA # It was clear to me that the teacher liked teaching us.
@@ -107,12 +108,12 @@ enthus4 <- IMPDAT$ST213Q04HA # The teacher showed enjoyment in teaching.
 
 enthus <- data.frame(enthus1,enthus2,enthus3,enthus4)
 mean.enthus <- apply(enthus,FUN = mean, MARGIN = 1, na.rm = TRUE)
-enthus_sc <- scale(mean.enthus)[,1]
-pisa18$ENTHUS <- ave(enthus_sc, school.id, FUN = function(x) mean(x, na.rm = TRUE))
+pisa18$enthus <- scale(mean.enthus)[,1]
+pisa18$ENTHUS <- ave(pisa18$enthus, school.id, FUN = function(x) mean(x, na.rm = TRUE))
 
 
-uni  <- IMPDAT$ST225Q06HA # Which of the following do you expect to complete?, ISCED level 5A or 6
-pisa18$UNI <- ave(uni, pisa18$school.id, FUN = function(x) mean(x, na.rm = TRUE))
+pisa18$uni  <- IMPDAT$ST225Q06HA # Which of the following do you expect to complete?, ISCED level 5A or 6
+pisa18$UNI <- ave(pisa18$uni, pisa18$school.id, FUN = function(x) mean(x, na.rm = TRUE))
 #cutoff <- quantile(pisa18$UNI, probs= 3/4)
 #pisa18$UNI01 <- as.numeric(pisa18$UNI > cutoff)
 
@@ -124,8 +125,8 @@ pisa18$UNI <- ave(uni, pisa18$school.id, FUN = function(x) mean(x, na.rm = TRUE)
 # 
 staff_lack     <- IMPDAT$SC017Q01NA # A lack of teaching staff.
 poor_staff     <- IMPDAT$SC017Q02NA # Inadequate or poorly qualified teaching staff.
-ass_staff_lack <- IMPDAT$SC017Q01NA # A lack of teaching staff.
-poor_ass_staff <- IMPDAT$SC017Q02NA # Inadequate or poorly qualified teaching staff.
+ass_staff_lack <- IMPDAT$SC017Q03NA # A lack of teaching staff.
+poor_ass_staff <- IMPDAT$SC017Q04NA # Inadequate or poorly qualified teaching staff.
 
 # EDUSHORT: pisa18$mat_lack,pisa18$poor_mat, pisa18$infra_lack, pisa18$poor_infra
 
@@ -137,15 +138,15 @@ poor_infra <- IMPDAT$SC017Q08NA # Inadequate or poor quality physical infrastruc
 
 staffshort.f <- data.frame(staff_lack, poor_staff) #, ass_staff_lack, poor_ass_staff
 mean.staffshort <- apply(staffshort.f,FUN = mean, MARGIN = 1, na.rm = TRUE)
-staffshort_sc <- scale(mean.staffshort)[,1]
-cutoff <- quantile(staffshort_sc, probs= 5/10)
-pisa18$STAFFLACK <- as.numeric(staffshort_sc > cutoff )
+staffshort <- scale(mean.staffshort)[,1]
+cutoff <- quantile(staffshort, probs= 5/10)
+pisa18$STAFFLACK <- as.numeric(staffshort > cutoff)
 
 
 edushort.f <- data.frame(mat_lack, poor_mat, infra_lack, poor_infra)# ass_staff_lack, poor_ass_staff)
 mean.edushort <- apply(edushort.f,FUN = mean, MARGIN = 1, na.rm = TRUE)
-edushort_sc <- scale(mean.edushort)[,1]
-pisa18$EDULACK <- as.numeric(edushort_sc > 0.0)
+edushort <- scale(mean.edushort)[,1]
+pisa18$EDULACK <- as.numeric(edushort > 0.0)
 
 # Competing school, 1 = There are two or more other schools in this area that compete for our students. 
 # 2 = There is one other school in this area that competes for our students., 
@@ -203,8 +204,9 @@ bul.yes <- rep(0,length(sum.bully))
 
 bul.yes[is.na(sum.bully)] <- NA
 bul.yes[sum.bully >= 2] <- 1
+pisa18$bull <- bul.yes
 
-pisa18$BULL <- ave(bul.yes, school.id, FUN = function(x) mean(x, na.rm = TRUE))
+pisa18$BULL <- ave(pisa18$bull, school.id, FUN = function(x) mean(x, na.rm = TRUE))
 
 
 # SENSE OF BELONGING # 1 = Strongly, 2= agree, 3 = disagree, agree, 4 = Strongly disagree
@@ -217,8 +219,8 @@ belong6 <- IMPDAT$ST034Q06TA     # I feel lonely at school.
 
 belong.f <- data.frame(belong1, belong2, belong3, belong4, belong5 , belong6)
 mean.belong <- apply(belong.f,FUN = mean, MARGIN = 1, na.rm = TRUE)
-belong_sc <- scale(mean.belong)[,1]
-pisa18$BELONG <- ave(belong_sc, school.id, FUN = function(x) mean(x, na.rm = TRUE))
+pisa18$belong <- scale(mean.belong)[,1]
+pisa18$BELONG <- ave(pisa18$belong, school.id, FUN = function(x) mean(x, na.rm = TRUE))
 
 # p. 79, participation
 part1  <- IMPDAT$ST062Q01TA # I <skipped> a whole school day.
@@ -227,8 +229,8 @@ part3  <- IMPDAT$ST062Q03TA # I arrived late for school. (not included)
 
 st_attendance <- data.frame(part1,part2)
 mean.st_attendance <- apply(st_attendance,FUN = mean, MARGIN = 1, na.rm = TRUE)
-st_attendance_sc <- scale(mean.st_attendance)[,1]
-pisa18$ATT <- ave(st_attendance_sc, school.id, FUN = function(x) mean(x, na.rm = TRUE))
+pisa18$att <- scale(mean.st_attendance)[,1]
+pisa18$ATT <- ave(pisa18$att, school.id, FUN = function(x) mean(x, na.rm = TRUE))
 
 # self-efficacy, 1  Strongly disagree, 4  Strongly agree
 self_eff1  <- IMPDAT$ST188Q01HA  # I usually manage one way or another.
@@ -239,8 +241,8 @@ self_eff5  <- IMPDAT$ST188Q07HA  # When I'm in a difficult situation, I can usua
 
 self_eff <- data.frame(self_eff1,self_eff2,self_eff3,self_eff4,self_eff5)
 mean.self_eff <- apply(self_eff,FUN = mean, MARGIN = 1, na.rm = TRUE)
-self_eff_sc <- scale(mean.self_eff)[,1]
-pisa18$RES <- ave(self_eff_sc, school.id, FUN = function(x) mean(x, na.rm = TRUE))
+pisa18$res <- scale(mean.self_eff)[,1]
+pisa18$RES <- ave(pisa18$res, school.id, FUN = function(x) mean(x, na.rm = TRUE))
 
 # enjoyment of reading, 1 = Strongly disagree, 4 = Strongly agree
 
@@ -253,8 +255,8 @@ joy_read5 <- 5 - IMPDAT$ST160Q05IA # I read only to get information that I need.
 
 joy_read <- data.frame(joy_read1,joy_read2,joy_read3,joy_read4,joy_read5)
 mean.joy_read <- apply(joy_read,FUN = mean, MARGIN = 1, na.rm = TRUE)
-joy_read_sc <- scale(mean.joy_read)[,1]
-pisa18$JOYREAD <- ave(joy_read_sc, school.id, FUN = function(x) mean(x, na.rm = TRUE))
+pisa18$joyread <- scale(mean.joy_read)[,1]
+pisa18$JOYREAD <- ave(pisa18$joyread, school.id, FUN = function(x) mean(x, na.rm = TRUE))
 
 # learning goals, 1 = Not at all true of me ...5 = Extremely true of me
 
@@ -264,8 +266,8 @@ goal3 <- IMPDAT$ST208Q04HA # My goal is to understand the content of my classes 
 
 goal.f <- data.frame(goal1, goal2, goal3)
 mean.goal <- apply(goal.f,FUN = mean, MARGIN = 1, na.rm = TRUE)
-goal_sc <- scale(mean.goal)[,1]
-pisa18$GOAL <- ave(goal_sc, school.id, FUN = function(x) mean(x, na.rm = TRUE))
+pisa18$goal <- scale(mean.goal)[,1]
+pisa18$GOAL <- ave(pisa18$goal, school.id, FUN = function(x) mean(x, na.rm = TRUE))
 
 # Value of school, 1 = Strongly agree,... 4 = Strongly disagree
 value1  <- IMPDAT$ST036Q05TA  # Trying hard at school will help me get a good job. 
@@ -274,8 +276,8 @@ value3  <- IMPDAT$ST036Q08TA  # Trying hard at school is important.
 
 value.f <- data.frame(value1, value2, value3)
 mean.value <- apply(value.f ,FUN = mean, MARGIN = 1, na.rm = TRUE)
-value_sc <- scale(mean.value)[,1]
-pisa18$VAL <- ave(value_sc, school.id, FUN = function(x) mean(x, na.rm = TRUE))
+pisa18$val <- scale(mean.value)[,1]
+pisa18$VAL <- ave(pisa18$val, school.id, FUN = function(x) mean(x, na.rm = TRUE))
 
 # motivation to master tasks, 1  Strongly disagree, 4  Strongly agree
 
@@ -286,8 +288,8 @@ mot4  <- IMPDAT$ST182Q06HA  # If I am not good at something, I would rather keep
                                    # master it than move on to something I may be good at.
 mot.f <- data.frame(mot1, mot2, mot3)
 mean.mot <- apply(mot.f ,FUN = mean, MARGIN = 1, na.rm = TRUE)
-mot_sc <- scale(mean.mot)[,1]
-pisa18$MOT <- ave(mot_sc, school.id, FUN = function(x) mean(x, na.rm = TRUE))
+pisa18$mot <- scale(mean.mot)[,1]
+pisa18$MOT <- ave(pisa18$mot, school.id, FUN = function(x) mean(x, na.rm = TRUE))
 
 # Meaning in life, 1 = Strongly disagree,... 4 = Strongly agree
 mean1  <- IMPDAT$ST185Q01HA  # My life has clear meaning or purpose. 
@@ -296,8 +298,8 @@ mean3  <- IMPDAT$ST185Q03HA  # I have a clear sense of what gives meaning to my 
 
 mean.f <- data.frame(mean1, mean2, mean3)
 mean.mean <- apply(mean.f ,FUN = mean, MARGIN = 1, na.rm = TRUE)
-mean_sc <- scale(mean.mean)[,1]
-pisa18$MEAN <- ave(mean_sc, school.id, FUN = function(x) mean(x, na.rm = TRUE))
+pisa18$mean <- scale(mean.mean)[,1]
+pisa18$MEAN <- ave(pisa18$mean, school.id, FUN = function(x) mean(x, na.rm = TRUE))
 
 
 # motivational and metacognitive variables
@@ -310,8 +312,8 @@ screadcomp3 <- IMPDAT$ST161Q03HA # I read fluently.
 
 screadcomp.f <- data.frame(screadcomp1, screadcomp2,  screadcomp3)
 screadcomp.mean <- apply(screadcomp.f,FUN = mean, MARGIN = 1, na.rm = TRUE)
-screadcomp_sc <- scale(screadcomp.mean)[,1]
-pisa18$COMP <- ave(screadcomp_sc, school.id, FUN = function(x) mean(x, na.rm = TRUE))
+pisa18$comp <- scale(screadcomp.mean)[,1]
+pisa18$COMP <- ave(pisa18$comp, school.id, FUN = function(x) mean(x, na.rm = TRUE))
 
 # SCREADDIFF # Self-concept of reading: Perception of difficulty
 # # 1 = Strongly disagree...4 = Strongly agree
@@ -321,8 +323,8 @@ screaddiff3 <- IMPDAT$ST161Q08HA # I find it difficult to answer questions about
 
 screaddiff.f <- data.frame(screaddiff1, screaddiff2,  screaddiff3)
 screaddiff.mean <- apply(screaddiff.f,FUN = mean, MARGIN = 1, na.rm = TRUE)
-screaddiff_sc <- scale(screaddiff.mean)[,1]
-pisa18$DIFF <- ave(screaddiff_sc, school.id, FUN = function(x) mean(x, na.rm = TRUE))
+pisa18$diff <- scale(screaddiff.mean)[,1]
+pisa18$DIFF <- ave(pisa18$diff, school.id, FUN = function(x) mean(x, na.rm = TRUE))
 
 
 # Is your school a public or a private school?
@@ -348,8 +350,8 @@ FEMS <- IMPDAT$SC002Q02TA
 pisa18$FEMFRAC <- FEMS/(FEMS + MALES)
 
 pisa18$NN_SC <- IMPDAT$SC048Q01NA/100 # Students whose <heritage language> is different from <test language>
-DISH  <- IMPDAT$SC048Q03NA/100 # Students from socioeconomically disadvantaged homes
-pisa18$DISHOME <- as.numeric(cut_number(DISH,2)) - 1
+pisa18$DISHOME  <- IMPDAT$SC048Q03NA/100 # Students from socioeconomically disadvantaged homes
+pisa18$DISH <- as.numeric(cut_number(pisa18$DISHOME,2)) - 1
 pisa18$SN_SC <- IMPDAT$SC048Q02NA/100 # Students with special needs
 pisa18$FAILED <- IMPDAT$SC164Q01HA/100 # in the last full academic year, what proportion of 
 # students in your school’s final grade left school without 
@@ -363,8 +365,8 @@ gfofail3 <- IMPDAT$ST183Q03HA # When I am failing, this makes me doubt my plans 
 
 gfofail.f <- data.frame(gfofail1, gfofail2, gfofail3)
 gfofail.mean <- apply(gfofail.f,FUN = mean, MARGIN = 1, na.rm = TRUE)
-gfofail_sc <- scale(gfofail.mean)[,1]
-pisa18$FOF <- ave(gfofail_sc, school.id, FUN = function(x) mean(x, na.rm = TRUE))
+pisa18$fof <- scale(gfofail.mean)[,1]
+pisa18$FOF <- ave(pisa18$fof, school.id, FUN = function(x) mean(x, na.rm = TRUE))
 
 # parents, enjoyment of reading, 1  Strongly disagree, 4  Strongly agree
 # enjoyment of reading, high values indicate a positive attitude towards reading
@@ -376,5 +378,5 @@ joyp_read5 <- 5 - IMPDAT$PA158Q05HA # I read only to get information that I need
 
 joyp_read <- data.frame(joy_read1,joy_read2,joy_read3,joy_read4,joy_read5)
 mean.joyp_read <- apply(joyp_read,FUN = mean, MARGIN = 1, na.rm = TRUE)
-joyp_read_sc <- scale(mean.joyp_read)[,1]
-pisa18$JOYPREAD <- ave(joyp_read_sc, school.id, FUN = function(x) mean(x, na.rm = TRUE))
+pisa18$joypread <- scale(mean.joyp_read)[,1]
+pisa18$JOYPREAD <- ave(pisa18$joypread, school.id, FUN = function(x) mean(x, na.rm = TRUE))
